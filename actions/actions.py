ca7a88b6e79d6099513613c149b1f0fab60f8194
hoaivu02 <134912650+hoaivu02@users.noreply.github.com>
2024-04-17 23:30:02 +0700

    Add greet, goodbye, thanks and dashboard function

--- actions/actions.py
+++ actions/actions.py
@@ -25,14 +25,3 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-
-class ActionClockIn(Action):
-    def name(self) -> Text:
-        return "action_clock_in"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_clock_in")
-        return []
